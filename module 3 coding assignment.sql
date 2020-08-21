b2d6368b2c2d3a5eaf9b1fc9d3fb809af57d2d18
VictorTaraba <55559682+VictorTaraba@users.noreply.github.com>
2020-08-21 14:56:42 +0300

    Update module 3 coding assignment.sql

--- module 3 coding assignment.sql
+++ module 3 coding assignment.sql
@@ -1,6 +1,16 @@
-#Using a subquery, find the names of all the tracks for the album "Californication"
+--- Using a subquery, find the names of all the tracks for the album "Californication"
 SELECT name FROM tracks
 WHERE AlbumId = (SELECT AlbumId
                 FROM Albums
                 WHERE Title = "Californication")
 LIMIT 8
+
+--- Find the total number of invoices for each customer along with the customer's full name, city and email
+SELECT c.firstname, 
+       c.lastname, 
+       c.city, 
+       c.email, 
+       i.CustomerId, 
+       COUNT(i.CustomerId) AS total_num_of_inv FROM invoices i
+INNER JOIN customers c ON c.CustomerId = i.CustomerId
+GROUP BY i.customerid
